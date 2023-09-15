211d46e03e0ffc057f7b9cc51bfcfe4489173eaf
Nguyễn Quang Anh <127297496+quanganh123@users.noreply.github.com>
2023-09-15 18:30:14 +0700

    init

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const router = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(express.urlencoded(
+//   extended: true
+// ));
+// app.use(express.join());
 
 app.use(morgan('combined')); 
 
@@ -15,13 +22,7 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+router(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const newRouter = require('./news');
+const siteRouter = require('./site');
+
+function router(app) {
+
+    app.use('/news' ,newRouter);
+
+    app.use('/', siteRouter);
+}
+
+module.exports = router;
